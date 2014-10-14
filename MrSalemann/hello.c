487bbe203cd48795e6567d98f1c7518c641190d3
LeoSalemann <leo.salemann@me.com>
2014-10-13 23:23:42 -0700

    Update hello.c
    Added some actual source code, should run in RobotC

--- MrSalemann/hello.c
+++ MrSalemann/hello.c
@@ -1,1 +1,13 @@
-/* Future hello.c file */
+/* RobotC Hellow World File */
+/* From http://byterevel.com/2011/09/28/robotc-tutorial-2-hello-world-and-programming */
+
+task main() //the start of all RobotC programs
+{// the start of the function
+  eraseDisplay();      //clear the NXT display
+  // now to display "Hello World"
+  nxtDisplayCenteredBigTextLine(2, "Hello");
+  nxtDisplayCenteredBigTextLine(4, "World!");
+  
+  while (true);
+    //loop until program is terminated (so we can read the display)
+}//the end of the function
