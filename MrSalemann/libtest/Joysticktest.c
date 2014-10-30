5bba22767a53971d6cbc49ec08c82c3f724f4bd9
LeoSalemann <leo.salemann@me.com>
2014-10-30 02:15:52 -0700

    Removed some commented-out code.

--- MrSalemann/libtest/Joysticktest.c
+++ MrSalemann/libtest/Joysticktest.c
@@ -9,9 +9,5 @@ task main()
   {
     getJoystickSettings(joystick);  // Update Buttons and Joysticks
     tankdrive(joystick.joy1_y1, joystick.joy1_y2);
-    /*
-    motor[motorC] = joystick.joy1_y1;
-    motor[motorB] = joystick.joy1_y2;
-    */
   }
 }
