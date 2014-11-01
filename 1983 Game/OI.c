4ef7d59744b69e32c17960de7f4bbd64b3a048f9
LeoSalemann <leo.salemann@me.com>
2014-11-01 14:37:29 -0700

    Update OI.c
    Added banner comments

--- 1983 Game/OI.c
+++ 1983 Game/OI.c
@@ -1,3 +1,10 @@
+/*
+OI (Operator Interface) is the only file that should be aware of joystick buttons and axes. 
+OI.c contains a collecton of "intuitive" functions like raiseArm(), lowerArm() so other developers
+don't have to worry about how the joystics are matched.  See test_arm.c for an example
+*/
+
+
 #include "JoystickDriver.c"
 bool lowerArm (TJoystick &joystick)
 {
