6429c7914c9734118a0e6fbcdee4f40f9e419d4f
almalimon <almalimon92599@yahoo.com>
2014-11-08 14:54:30 -0800

    Update Autonomous Route 1.c

--- 1983 Game/Autonomous Route 1.c
+++ 1983 Game/Autonomous Route 1.c
@@ -1,1 +1,19 @@
-/* this is a file*/
+#include "latch.c"
+#include "drivebase.c"
+
+
+task main()
+{
+	latch1_open (); //latch opens to "intitialize"
+	tankdrive (100, 100); //forewards
+	wait1Msec (4000);
+	tankdrive (-100,100); //turn 180
+	wait1Msec (3000);
+	tankdrive (-100,-100); //backwards
+	wait1Msec (2000);
+	latch1_close(); //attaches to goal
+	tankdrive (-100,100); //turn 40
+	wait1Msec (1000);
+	tankdrive (100,100);//foreward
+	wait1Msec (10000);
+}
