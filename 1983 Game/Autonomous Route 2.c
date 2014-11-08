9a44ee20286b3a197907dcd910926b1df9cdb523
almalimon <almalimon92599@yahoo.com>
2014-11-08 14:54:58 -0800

    Update Autonomous Route 2.c

--- 1983 Game/Autonomous Route 2.c
+++ 1983 Game/Autonomous Route 2.c
@@ -1,1 +1,13 @@
-/* this is a file*/
+#include "drivebase.c"
+task main()
+{
+	tankdrive (100,100);
+	wait1Msec (4000);
+	tankdrive (100, 25);
+	wait1Msec (3000);
+	tankdrive (80,80);
+	wait1Msec (3000);
+	tankdrive (50, 100);
+	wait1Msec (8000);
+
+}
