f6f34ddeb3d08ac8c2de86ea3cf4b42a6f3e8a8d
almalimon <almalimon92599@yahoo.com>
2014-11-12 17:15:22 -0800

    Update Autonomous Route 1.c
    adds wait_seconds function defined in drivebase.c

--- 1983 Game/Autonomous Route 1.c
+++ 1983 Game/Autonomous Route 1.c
@@ -6,14 +6,14 @@ task main()
 {
 	latch1_open (); //latch opens to "intitialize"
 	tankdrive (100, 100); //forewards
-	wait1Msec (4000);
+	wait_seconds (12);
 	tankdrive (-100,100); //turn 180
-	wait1Msec (3000);
+	wait_seconds (3);
 	tankdrive (-100,-100); //backwards
-	wait1Msec (2000);
+	wait_seconds (2);
 	latch1_close(); //attaches to goal
 	tankdrive (-100,100); //turn 40
-	wait1Msec (1000);
-	tankdrive (100,100);//foreward
-	wait1Msec (10000);
+	wait_seconds (1);
+	tankdrive (100,100);//forewardx
+	wait_seconds (10);
 }
