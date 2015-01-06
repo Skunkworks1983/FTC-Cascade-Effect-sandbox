74f9793745f1d7e05319b5f1235cb3238ec0e05e
LeoSalemann <leo.salemann@me.com>
2015-01-06 15:12:46 -0800

    web-based edit.

--- MrSalemann/YetAnotherTest.c
+++ MrSalemann/YetAnotherTest.c
@@ -2,5 +2,6 @@
 Yet another sample file.  I know, right?  I just gotta document the procedure for once.
 one more line.
 
-Added a new line. Using TortoiseGIT for checkout/checkin.
+Added a new line. Using TortoiseGIT  checkout/checkin.
+Added a new line, via Git over the web.  Also deleted a word in the line above.
 */
